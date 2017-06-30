aec9f51a0d82bd13e47faac4b816e7f32c9b70cd
Harshad Deo <subterranean.hominid@gmail.com>
2017-06-30 13:28:39 +0530

    bump scalanative version to 0.3

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,5 +1,5 @@
 object Settings {
-  lazy val version = "0.2.2"
+  lazy val version = "0.3-SNAPSHOT"
   lazy val scalaVersion = "2.11.11"
   lazy val crossScalaVersions = Seq("2.11.8", "2.11.11")
 }
