f741dbca9ac1bb32a9edbf5cee1b71b195f7b444
Harshad Deo <subterranean.hominid@gmail.com>
2017-09-01 12:37:25 +0530

    cleanup runner

--- src/main/scala/sntb/SntbFramework.scala
+++ src/main/scala/sntb/SntbFramework.scala
@@ -1,6 +1,7 @@
 package sntb
 
-import sbt.testing.{Fingerprint, Framework, Runner, SubclassFingerprint}
+import org.scalajs.testinterface.TestUtils
+import sbt.testing.{EventHandler, Fingerprint, Framework, Logger, Runner, SubclassFingerprint, Task, TaskDef}
 
 final class SntbFramework extends Framework {
   override final def name = "sntb"
@@ -10,6 +11,30 @@ final class SntbFramework extends Framework {
       override final def isModule = false
       override final def requireNoArgConstructor = true
     })
-  override final def runner(args: Array[String], remoteArgs: Array[String], testClassLoader: ClassLoader): Runner =
-    new SntbRunner(args, remoteArgs, testClassLoader)
+  override final def runner(ags: Array[String], ras: Array[String], testClassLoader: ClassLoader): Runner =
+    new Runner {
+
+      override final def args: Array[String] = ags
+
+      override final def remoteArgs: Array[String] = ras
+
+      override final def done(): String =
+        "----------------------------------- Done -----------------------------------"
+
+      override final def tasks(taskDefs: Array[TaskDef]): Array[Task] = taskDefs.map(makeTask)
+
+      private def makeTask(td: TaskDef): Task = {
+        new Task {
+          override final def taskDef: TaskDef = td
+          override final def execute(eventHandler: EventHandler, loggers: Array[Logger]): Array[Task] = {
+            TestUtils
+              .newInstance(taskDef.fullyQualifiedName, testClassLoader, Nil)(Nil)
+              .asInstanceOf[TestProvider]
+              .run()
+            Array()
+          }
+          override final def tags(): Array[String] = Array()
+        }
+      }
+    }
 }
